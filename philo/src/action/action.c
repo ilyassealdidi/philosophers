8cdc273c324db47abae4ec36fb6784c05b3107f1
Ilyasse Aldidi <ilyassealdidi@gmail.com>
2024-05-01 10:19:25 +0100

    updates

--- philo/src/action/action.c
+++ philo/src/action/action.c
@@ -6,7 +6,7 @@
 /*   By: ialdidi <ialdidi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 22:12:24 by ialdidi           #+#    #+#             */
-/*   Updated: 2024/04/25 15:43:36 by ialdidi          ###   ########.fr       */
+/*   Updated: 2024/04/30 22:49:16 by ialdidi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*action(void *param)
 		;
 	philo->last_eating_time = get_current_time();
 	if (philo->id % 2 == 1)
-		sleeper(50);
+		sleeper(philo->_parent->settings.time_to_eat);
 	while (1)
 	{
 		if (eat(philo) == 0 || bedtime(philo) == 0 || think(philo) == 0)
